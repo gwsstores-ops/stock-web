e896d2a56ca7300fe0a8f4309c8fc3bdd02ba596
Stores13 <stores13@gwscork.ie>
2026-02-25 16:20:33 +0000

    Fix IOS home screen

---
+++ src/app/head.tsx
@@ -0,0 +1,16 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="theme-color" content="#ffffff" />
+
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link
+        rel="apple-touch-icon-precomposed"
+        sizes="180x180"
+        href="/apple-touch-icon-precomposed.png"
+      />
+    </>
+  );
+}
