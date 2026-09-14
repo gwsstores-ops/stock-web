1eedb1e8c61ab5c3bebea0101cd8fe24f44a522f
Stores13 <stores13@gwscork.ie>
2026-09-14 12:40:03 +0100

    Revert "Add Containers tab for CSV row entry"
    This reverts commit 05afe9d32acbcd4d2f4b7eb90d1b5b346793c809.

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -16,11 +16,6 @@ const items = [
     icon: "/qr-icon.png"
   },
   {
-    href: "/containers",
-    label: "Containers",
-    icon: "/container.png"
-  },
-  {
     href: "/move",
     label: "Move",
     icon: "/move-icon.png"
